b516129bc05e2d1df09e624d1fce04c3e58ab1e2
John Salas <johndsalas@gmail.com>
2021-02-18 18:02:30 -0600

    modeling finished next write conclutions

--- modeling.py
+++ modeling.py
@@ -9,6 +9,8 @@ from statsmodels.tsa.api import Holt, ExponentialSmoothing
 from math import sqrt
 from sklearn.metrics import mean_squared_error
 
+from fbprophet import Prophet
+
 def append(model_type, target_var, rmse, eval_df):
     d = {'model_type': [model_type], 'target_var': [target_var], 'rmse': [rmse]}
     d = pd.DataFrame(d)
@@ -22,7 +24,7 @@ def plot_and_eval(train, validate, yhat, target_var, model_type):
     plt.plot(validate[target_var], label = 'Validate', linewidth = 1)
     plt.plot(yhat[target_var])
     plt.title(target_var)
-    rmse = round(sqrt(mean_squared_error(validate[target_var], yhat[target_var])), 4)
+    rmse = round(sqrt(mean_squared_error(validate[target_var], yhat[target_var])), 2)
     print(model_type, f'-- RMSE: {rmse}')
     plt.show()
 
@@ -47,7 +49,7 @@ def simple_average(train, validate, target_var, eval_df):
 
     model_type = "Simple Average"
 
-    temps = round(train[target_var].mean(),4)
+    temps = round(train[target_var].mean(),2)
     yhat = pd.DataFrame({target_var: [temps]}, index = validate.index)
 
     rmse = plot_and_eval(train, validate, yhat, target_var, model_type)
@@ -66,7 +68,7 @@ def moving_average(train, validate, target_var, eval_df):
 
         model_type = f"{label[index]} Moving Average "
      
-        temps = round(train[target_var].rolling(period).mean().iloc[-1],4)
+        temps = round(train[target_var].rolling(period).mean().iloc[-1],2)
         yhat = pd.DataFrame({target_var: [temps]}, index = validate.index)
        
         rmse = plot_and_eval(train, validate, yhat, target_var, model_type)
@@ -137,3 +139,31 @@ def get_baseline(train, validate, target_var, eval_df):
     eval_df = moving_average(train, validate, target_var, eval_df)
 
     return eval_df
+
+def get_prophet(train, validate, target_var, eval_df):
+
+    model_type = "Prophet"
+
+    # get training dataframe for profit
+    train_prophet = train.reset_index().rename(columns = {"dt":"ds", target_var:"y"})
+    train_prophet['ds'] = pd.to_datetime(train_prophet['ds'])
+
+    # get validation range for prophet
+    validate_prophet = validate.reset_index()
+    validate_prophet = validate_prophet.rename(columns = {'dt':'ds'})
+    validate_prophet= validate_prophet[['ds']]
+
+    # define and fit the model
+    model = Prophet()
+    model.fit(train_prophet)
+
+    # use the model to make a forecast
+    forecast = model.predict(validate_prophet)
+
+    # get variables for plot and eval and append
+    yhat = forecast[['ds','yhat']]
+    yhat = yhat.rename(columns={'yhat':target_var}, index = forecast.ds)
+
+    rmse = plot_and_eval(train, validate, yhat, target_var, model_type)
+
+    eval_df = append(model_type, target_var, rmse, eval_df)
